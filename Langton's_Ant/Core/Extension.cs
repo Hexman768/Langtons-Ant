ad503cb7fe8ef7148ebf570d74efb88341175dab
Zachary Pedigo <Zachary.Pedigo@Cerner.com>
2022-02-02 23:53:17 -0600

    Added documentation in Extension class

--- Langton's_Ant/Core/Extension.cs
+++ Langton's_Ant/Core/Extension.cs
@@ -3,8 +3,16 @@ using System.Windows.Forms;
 
 namespace Langton_s_Ant.Core
 {
+    /// <summary>
+    /// This class is used to call the static method below to stop screen flickering.
+    /// </summary>
     public static class Extension
     {
+        /// <summary>
+        /// This method enables double buffering which prevents the Control from flickering.
+        /// </summary>
+        /// <param name="control">Control to modify</param>
+        /// <param name="enabled">Boolean value</param>
         public static void DoubleBuffered(this Control control, bool enabled)
         {
             var prop = control.GetType().GetProperty("DoubleBuffered", BindingFlags.Instance | BindingFlags.NonPublic);
